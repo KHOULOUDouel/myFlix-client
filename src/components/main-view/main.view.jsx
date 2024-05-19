e30aef441f3b0302660d230b649c30281ac0e87a
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-19 21:49:54 +0200

    Update main.view.jsx

--- src/components/main-view/main.view.jsx
+++ src/components/main-view/main.view.jsx
@@ -1,38 +1,39 @@
 import React, { useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
 export const MainView = () => {
   const [movies, setMovies] = useState([
     {
-        id: 1,
-        title: "Inception",
-        image: "https://image-url-1.com",
-        director: "Christopher Nolan"
-      },
-      {
-        id: 2,
-        title: "The Matrix",
-        image: "https://image-url-2.com",
-        director: "The Wachowskis"
-      },
-      {
-        id: 3,
-        title: "Interstellar",
-        image: "https://image-url-3.com",
-        director: "Christopher Nolan"
-      },
-      {
-        id: 4,
-        title: "The Dark Knight",
-        image: "https://image-url-4.com",
-        director: "Christopher Nolan"
-      },
-      {
-        id: 5,
-        title: "Fight Club",
-        image: "https://image-url-5.com",
-        director: "David Fincher"
-      }
+      id: 1,
+      title: "Inception",
+      image: "https://image-url-1.com",
+      director: "Christopher Nolan"
+    },
+    {
+      id: 2,
+      title: "The Matrix",
+      image: "https://image-url-2.com",
+      director: "The Wachowskis"
+    },
+    {
+      id: 3,
+      title: "Interstellar",
+      image: "https://image-url-3.com",
+      director: "Christopher Nolan"
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      image: "https://image-url-4.com",
+      director: "Christopher Nolan"
+    },
+    {
+      id: 5,
+      title: "Fight Club",
+      image: "https://image-url-5.com",
+      director: "David Fincher"
+    }
   ]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
