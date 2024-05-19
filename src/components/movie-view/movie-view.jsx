7a63811419f117fa145c76772b281905c64a9612
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-19 21:56:07 +0200

    Make sure the MovieView component is set up to display the detailed information of the selected movie

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,18 +1,19 @@
+import React from 'react';
+
 export const MovieView = ({ movie }) => {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <img src={movie.image} alt={movie.title} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
+        <img src={movie.image} alt={movie.title} />
       </div>
-    );
-  };
-  
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+    </div>
+  );
+};
