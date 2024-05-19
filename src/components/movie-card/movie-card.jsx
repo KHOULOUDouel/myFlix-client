a8d6771855a8ad0f839a71508fc35bdad6a5c744
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-05-19 21:48:32 +0200

    Add a click event to each MovieCard to set the selected movie when clicked

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,8 @@
-export const MovieCard = ({ movie }) => {
-    return <div>{movie.title}</div>;
+export const MovieCard = ({ movie, onMovieClick }) => {
+    return (
+      <div onClick={() => onMovieClick(movie)}>
+        {movie.title}
+      </div>
+    );
   };
   
