aa61b606daaa5d5ee300aa2597140624cf4586fb
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-08 18:49:48 +0200

    create a new LoginView function component

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,19 @@
+// src/components/login-view/login-view.jsx
+
+import React from 'react';
+
+export const LoginView = () => {
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
