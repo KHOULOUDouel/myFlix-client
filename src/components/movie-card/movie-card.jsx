22ac35c16871ea2b10742d62cdd8bbb36e2e3697
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-09 21:45:07 +0200

    Update MovieCard to Use Bootstrap Components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,31 @@
+// src/components/movie-card/movie-card.jsx
+
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movie.poster} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>Director: {movie.director}</Card.Text>
+        <Button variant="primary" onClick={() => onMovieClick(movie)}>
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    director: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
+
