fa33416bd0bed5429f38eaafc199757d33737f48
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-13 13:40:39 +0200

    Wrap the App in a <Container>

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,20 @@ import { createRoot } from 'react-dom/client';
 // Import the MainView component from the specified path
 import { MainView } from './components/main-view/main-view.jsx';
 
+// Import Container from React Bootstrap
+import Container from 'react-bootstrap/Container';
 
 // Import statement to include the styles from `./index.scss`
 import "./index.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Define the main App component, which will eventually include all other components
 const App = () => {
-  return <MainView />;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
 // Select the root DOM element where the React app will be rendered
