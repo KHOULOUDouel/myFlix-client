2a5e6f3b2c1589638c2afb9815394a656ac8f2db
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-13 14:10:22 +0200

    Refactor MovieCard component to use React Bootstrap's card components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card onClick={() => onMovieClick(movie)} style={{ cursor: 'pointer' }}>
+      <Card.Img variant="top" src={movie.image} alt={movie.title} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director}</Card.Text>
+        <Button variant="link" onClick={() => onMovieClick(movie)}>
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
-// Define PropTypes for MovieCard
+
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    director: PropTypes.string
+    director: PropTypes.string,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  onMovieClick: PropTypes.func.isRequired,
 };
