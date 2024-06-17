8acbdf0973ee055e0d6a0896a0fe65d556c8d04f
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-17 12:53:35 +0200

    Remove the line import '~bootstrap/scss/bootstrap.scss';

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ import Container from 'react-bootstrap/Container';
 
 // Import statement to include the styles from `./index.scss`
 import "./index.scss";
-import '~bootstrap/scss/bootstrap.scss';
+
 
 // Define the main App component, which will eventually include all other components
 const App = () => {
