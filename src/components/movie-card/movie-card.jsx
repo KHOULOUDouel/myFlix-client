5e8d795e0d960a741be46e57aae56603258bf7af
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-19 01:03:16 +0200

    Add a Favorite Button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,7 +4,9 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, user, onFavorite }) => {
+  const isFavorite = user && user.FavoriteMovies.includes(movie._id);
+
   return (
     <Card>
       <Card.Img variant="top" src={movie.imagePath} alt={movie.title} />
@@ -14,6 +16,14 @@ export const MovieCard = ({ movie }) => {
         <Button as={Link} to={`/movies/${movie._id}`} variant="link">
           Open
         </Button>
+        {user && (
+          <Button
+            variant={isFavorite ? "danger" : "success"}
+            onClick={() => onFavorite(movie._id)}
+          >
+            {isFavorite ? "Unfavorite" : "Favorite"}
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
@@ -35,5 +45,8 @@ MovieCard.propTypes = {
       description: PropTypes.string,
     }).isRequired,
   }).isRequired,
+  user: PropTypes.shape({
+    FavoriteMovies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }),
+  onFavorite: PropTypes.func.isRequired,
 };
-
