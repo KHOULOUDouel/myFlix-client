f4b0dacfd2d237f52558a1ba8e413f2367d5906f
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-19 00:46:35 +0200

    Update index.jsx to Use MainView Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { MainView } from './components/main-view/main-view';
-import { MovieView } from './components/movie-view/movie-view';
-import { SignupView } from './components/signup-view/signup-view';
-import { LoginView } from './components/login-view/login-view';
-import Container from 'react-bootstrap/Container';
 import './index.scss';
 
 const App = () => {
   return (
     <Router>
-      <Container>
-        <Routes>
-          <Route path="/" element={<MainView />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/signup" element={<SignupView />} />
-          <Route path="/movies/:movieId" element={<MovieView />} />
-        </Routes>
-      </Container>
+      <MainView />
     </Router>
   );
 };
