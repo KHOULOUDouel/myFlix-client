4e138befcb76f8093a4953154a347d5c887b74f3
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-19 00:31:43 +0200

    Replace buttons that use onMovieClick and onBackClick with React Router links

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card onClick={() => onMovieClick(movie)} style={{ cursor: 'pointer' }}>
+    <Card>
       <Card.Img variant="top" src={movie.imagePath} alt={movie.title} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>
-          <strong>Director:</strong> {movie.director.name}
-          <br />
-          <strong>Genre:</strong> {movie.genre.name}
-        </Card.Text>
-        <Button variant="link" onClick={() => onMovieClick(movie)}>
+        <Card.Text>{movie.director.name}</Card.Text>
+        <Button as={Link} to={`/movies/${movie._id}`} variant="link">
           Open
         </Button>
       </Card.Body>
@@ -24,7 +21,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
@@ -32,13 +29,11 @@ MovieCard.propTypes = {
       name: PropTypes.string.isRequired,
       bio: PropTypes.string,
       birth: PropTypes.string,
-      death: PropTypes.string,
     }).isRequired,
     genre: PropTypes.shape({
       name: PropTypes.string.isRequired,
       description: PropTypes.string,
     }).isRequired,
-    featured: PropTypes.bool,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
+
