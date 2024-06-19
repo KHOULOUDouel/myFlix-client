1c700b0dd9b64a787fa07badab7e650f9931ae87
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-06-19 02:04:27 +0200

    Ensure there is only one <Router> component in my application, located in the index.jsx file.

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+// Import the createRoot function from the 'react-dom/client' package
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { MainView } from './components/main-view/main-view';
-import './index.scss';
+import Container from 'react-bootstrap/Container';
 
+// Import the MainView component from the specified path
+import { MainView } from './components/main-view/main-view';
+
+// Import statement to include the styles from `./index.scss`
+import "./index.scss";
+
+// Define the main App component, which will eventually include all other components
 const App = () => {
   return (
     <Router>
-      <MainView />
+      <Container>
+        <MainView />
+      </Container>
     </Router>
   );
 };
 
+// Select the root DOM element where the React app will be rendered
 const container = document.querySelector("#root");
+
+// Create a root for React rendering using the selected DOM element
 const root = createRoot(container);
+
+// Render the App component into the root DOM element
 root.render(<App />);
